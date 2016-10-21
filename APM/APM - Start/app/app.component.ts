0f184128d947a0c12db5e0e0cc44601b4726b1be
Lillian Pham <lillian.l.pham@gmail.com>
2016-10-21 17:34:46 -0400

    edit app component content

--- APM/APM - Start/app/app.component.ts
+++ APM/APM - Start/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
-    selector: 'pm-app',
-    template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+@Component ({
+	selector: 'pm-app',
+	template: `
+	<div><h1>{{pageTitle}}</h1>
+		<div>My First Component</div>
+	</div>
+	`
 })
-export class AppComponent { }
+export class AppComponent {
+	pageTitle: string = 'Acme Product Mangement';
+}
+
