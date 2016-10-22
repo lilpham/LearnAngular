14c94be62693261463d9662678f54a0d47fd09fd
Lillian Pham <lillian.l.pham@gmail.com>
2016-10-22 11:19:26 -0400

    created stylesheet for an element and defined the property

--- APM/APM - Start/app/products/product-list.component.ts
+++ APM/APM - Start/app/products/product-list.component.ts
@@ -5,6 +5,7 @@ import { IProduct } from './product';
 @Component({
 	selector: 'pm-products',
 	templateUrl: 'app/products/product-list.component.html'
+	styleUrls: ['app/products/product-list.component.css']
 })
 export class ProductListComponent { 
 	pageTitle: string = 'Product List';
