a4a700ff7dccb26be441ae531387e47c87b5c152
Lillian Pham <lillian.l.pham@gmail.com>
2016-10-23 21:54:10 -0400

    add http service

--- APM/APM - Start/app/products/product-list.component.ts
+++ APM/APM - Start/app/products/product-list.component.ts
@@ -14,6 +14,8 @@ export class ProductListComponent implements OnInit {
 	imageMargin: number = 2;
 	showImage: boolean = false;
 	listFilter: string;
+	errorMessage: string;
+
 	products: IProduct[]; 
 
 	constructor(private _productService: ProductService){
@@ -24,8 +26,11 @@ export class ProductListComponent implements OnInit {
 		this.showImage = !this.showImage;
 	}
 	ngOnInit(): void {
-		this.products = this._productService.getProducts();
+		this._productService.getProducts()
+			.subscribe(products => this.products = products,
+						error => this.errorMessage = <any>error);
 	}
+
 	onRatingClicked(message: string): void {
 		this.pageTitle = 'Product List:' + message;
 	}
